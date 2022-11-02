582e9ce44e079d91842d7b170542be644a6ac063
CHEN <chen@gmail.com>
2022-11-02 18:31:38 +0800

    test1

---
+++ temp.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!   脚本描述
+// @author       You
+// @match        http://*/*            生效处
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==   图标
+// @grant        none
+// ==/UserScript==
+
+//原信息，仅提供给tempermonkey使用
+
+(function () {
+  "use strict";
+  console.log("hello world");
+  // Your code here...
+})();
